cf01d50a31dce575cb945de753750a5eaa9b3372
joshuarichter115 <106352280+joshuarichter115@users.noreply.github.com>
2022-11-26 15:06:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,131 @@
-//Then
-const emailRef = document.querySelector(".email")
-const statusRef = document.querySelector(".status")
-
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     emailRef.innerHTML = data.email;
-//   });
-
-// Async Await
-
-async function main() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-    const data = await response.json()
-    emailRef.innerHTML = data.email;
-
+function sumOfTwoNumbers(number1, number2) {
+    let sum = number1 + number2;
+    return sum;
 }
 
-main();
+console.log(sumOfTwoNumbers(1, 10));
 
-function getVideo (subscriptionStatus) {
-    return new Promise ((resolve, reject) => {
-        if (subscriptionStatus === "VIP") {
-            resolve("Show video");
-        }
-        else if (subscriptionStatus === "FREE") {
-            resolve("Show trailer");
-        }
-        else {
-            reject("no video");
-        }
-    })
+function hoursIntoSeconds(hours) {
+    let seconds = hours*3600
+    return seconds;
 }
 
-async function main1(){
-    const status = await getVideo("VIP");
-    statusRef.innerHTML = status;
+console.log(hoursIntoSeconds(10));
+
+function calcPerimeter (length, width) {
+    let perimeter = 2*length + 2*width;
+    return perimeter;
 }
 
-main1();
+console.log(calcPerimeter(2, 9));
+
+function appendFrontEnd (string) {
+    let newString = string+"Frontend";
+    return newString
+}
+
+console.log(appendFrontEnd("Apple"));
+
+function sumGreaterThanHundred(num1, num2) {
+    let sum1 = num1 + num2
+    if (sum1 > 100) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(sumGreaterThanHundred(60, 50));
+
+function lessThanOrEqualToZero (testNum) {
+    if (testNum <= 0) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(lessThanOrEqualToZero(-3));
+
+function oppositeBoolean(boolean) {
+    if (boolean === true) {
+        return false;
+    }
+
+    else {
+        return true;
+    }
+}
+
+console.log(oppositeBoolean(true));
+
+function isNotZero(exNum) {
+    if (exNum !== 0) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(isNotZero(5));
+
+function calcRemainder (numb1, numb2) {
+    let remainder = numb1 % numb2
+    return remainder
+}
+
+console.log(calcRemainder(4, 2));
+
+function isOdd (maybeOddNum) {
+    if (maybeOddNum % 2 !== 0) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(isOdd(4));
+
+function booleanInteger(number) {
+    if (number % 2 === 0) {
+        return 1;
+    }
+
+    else {
+        return -1;
+    }
+}
+
+console.log(booleanInteger(6));
+
+function isLoggedInAndSubscribed (logStatus, subStatus) {
+    if (logStatus === "LOGGED_IN" && subStatus === "SUBSCRIBED") {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"));
+
+function isLoggedInOrSubscribed(loggedIn, subbedIn) {
+    if (loggedIn === "LOGGED_IN" || subbedIn === "SUBSCRIBED") {
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+console.log(isLoggedInOrSubscribed("LOGGED_IN", "SUBSCRIBED"));
