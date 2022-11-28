d8e1da10add27cd67bed4438d3342ef10f7fb16e
joshuarichter115 <106352280+joshuarichter115@users.noreply.github.com>
2022-11-27 21:08:40 -0500

    JavaScript Top Difficulty Problems

--- app.js
+++ app.js
@@ -1,151 +1,43 @@
-function falsyOrTruthy (elem1, elem2) {
-    if (!elem1){
-        return elem1;
-    }
+function showRating (ratingNumber) {
+    let ratings = "";
+    for (i = 1; i<=ratingNumber; i++) {
+        if (i === ratingNumber) {
+            ratings = ratings + "*"
+        }
 
-    else{
-        return elem2;
-    }
-}
-
-console.log(falsyOrTruthy(1, 2));
-
-function arrLength (arr) {
-    let arrayLength = arr.length;
-    return arrayLength;
-}
-
-console.log(arrLength([1, 2, 3, 4]));
-
-function lastElem (arr1) {
-    let i = arr1.length - 1
-    return arr1[i]
-}
-
-console.log(lastElem([0, 1, 2, "z"]));
-
-
-let sum = 0;
-function arrSum (arr2) {
-    for (i = 0; i<=arr2.length-1; i++) {
-        sum = sum + arr2[i];
-    }
-    return sum;
-}
-
-console.log(arrSum([1, 2, 3]));
-
-let sum1 = 0;
-let arr3 = [];
-
-function progressiveSum (num1) {
-    for (i = 0; i <= num1; i++) {
-        arr3.push(i);
-    }
-    for (i = 0; i<=arr3.length-1; i++) {
-        sum1 = sum1 + arr3[i]
-    }
-    return sum1;
-
-}
-
-console.log(progressiveSum(600));
-
-function calcTime (seconds) {
-    let timerMinutes = Math.trunc(seconds/60);
-    let timerSeconds = seconds % 60;
-    if (timerSeconds.toString().length === 1 ) {
-        timerSeconds = timerSeconds + "0";
-    }
-    if (timerMinutes.toString().length === 1) {
-        timerMinutes = "0" + timerMinutes;
-    }
-    return timerMinutes+":"+timerSeconds;
-}
-
-console.log(calcTime(60));
-
-function getMax (arr4) {
-    let max = arr4[0];
-    for (i = 0; i<=arr4.length-1; i++) {
-        if (arr4[i] > max) {
-            max = arr4[i]
-    }
-    return max;
-}
-}
-
-console.log(getMax([12, 10, -20]));
-
-function reverseArray (arr5) {
-    let backwardsArr = [];
-    for (i = arr5.length-1; i>=0; i--) {
-        backwardsArr.push(arr5[i]);
-    }
-    return backwardsArr;
-}
-
-console.log(reverseArray([0, 1, 2, 3, 1]));
-
-function reverseString (str1) {
-    let backwardsString = "";
-    for (i = str1.length-1; i>=0; i--) {
-        backwardsString = backwardsString + str1[i];
-    }
-    return backwardsString;
-}
-
-console.log(reverseString("This is cool"));
-
-function convertToZeroes (arr6) {
-    for (i = 0; i<=arr6.length-1; i++) {
-        arr6[i] = 0;
-    }
-    return arr6;
-}
-
-console.log(convertToZeroes([1, 2, 3, 4, 5, 6]));
-
-function removeApples (arr7) {
-    for (i = 0; i<=arr7.length-1; i++) {
-        if (arr7[i] === "Apple") {
-            arr7.splice(i, 1);
+        else {
+            ratings = ratings + "* "
         }
     }
-    return arr7;
-}
-
-console.log(removeApples(["Apple", "Banana", "Orange", "Apple", "Banana"]));
-
-function filterOutFalsy (arr8) {
-    for (i = 0; i<=arr8.length-1; i++) {
-        if (!arr8[i]) {
-            arr8.splice(i, 1);
-        }
+    if (!Number.isInteger(ratingNumber)){
+        ratings = ratings + ".";
     }
-    return arr8;
+    return ratings;
 }
 
-console.log(filterOutFalsy(["Tomato", "Orange", false]));
+console.log(showRating(4));
 
-function showTruthy (arr9) {
-    let truthyArr = [];
-    for (i = 0; i<=arr9.length-1; i++) {
-        if (!!arr9[i]) {
-            truthyArr.push(arr9[i])
-        }
-    }
-    return truthyArr;
+function sortLowToHigh(arr1) {
+    return arr1.sort((a, b) => a - b)
 }
 
-console.log(showTruthy(["Tomato", "Orange", false]));
+console.log(sortLowToHigh([1, 5, 0, 10, 100015, 2000, 2]));
 
-function convertToBoolean (arr10) {
-    let boolArr = [];
-    for (i = 0; i<=arr10.length-1; i++) {
-        boolArr.push(!!arr10[i])
-    }
-    return boolArr;
+async function postsByUsers (userId) {
+    const promise = await fetch("https://jsonplaceholder.typicode.com/posts/")
+    const result = await promise.json()
+    const posts = result.filter(element => element.userId === userId)
+    console.log(posts)
 }
 
-console.log(convertToBoolean([500, 0, "David", "", []]));
+postsByUsers(4);
+
+async function firstXIncomplete (exnumber) {
+    const promise1 = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const results1 = await promise1.json()
+    const posts1 = results1.filter(element1 => element1.completed === false).slice(0, exnumber)
+    console.log(posts1)
+
+}
+
+firstXIncomplete(4);
